227d02e431032a2cad8fe093e192cab8e4f33122
qgsoft <55126716+qgsoft@users.noreply.github.com>
2020-05-11 12:19:06 +0800

    Update data.js

--- static/data.js
+++ static/data.js
@@ -1,7 +1,7 @@
 
 {
   "enable":true,
-  "version":3,
+  "version":1,
   "forceupdate":true,
   "msg":"支持爱奇艺、腾讯、优酷视频(建议老用户安装后首次运行会自动更新脚本。退出后再次启动APP即可)",
   "downurl":"http://114.116.6.29/public/QGBrowser.apk"
