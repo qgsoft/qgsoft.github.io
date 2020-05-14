3374c323f403c973537213952a7e9045fbcdc622
qgsoft <55126716+qgsoft@users.noreply.github.com>
2020-05-14 15:26:52 +0800

    Update data.js

--- static/data.js
+++ static/data.js
@@ -1,8 +1,8 @@
 
 {
   "enable":true,
-  "version":3,
+  "version":5,
   "forceupdate":true,
-  "msg":"支持爱奇艺、腾讯、优酷视频(建议老用户安装后首次运行会自动更新脚本。退出后再次启动APP即可)",
+  "msg":"提升稳定性",
   "downurl":"http://114.116.6.29/api/download"
 }
